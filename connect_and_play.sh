c504a222b65ed9576d1af8af596d5772997339fb
syd-shelby <sshelby3@vols.utk.edu>
2020-02-25 03:29:03 +0000

    close to working

--- connect_and_play.sh
+++ connect_and_play.sh
@@ -2,7 +2,7 @@
 
 sudo systemctl start bluetooth
 # pulseaudio -k
-pulseaudio -start
-echo 'power on\n discoverable on\n pairable on\n pair 30:23:D6:BE:DC:9B\n connect 30:23:d6:BE:DC:9B\n quit\n' | bluetoothctl
-mpg123 rockytop.mp3
+pulseaudio --start
+echo 'power on\n discoverable on\n pairable on\n connect 30:23:D6:BE:DC:9B\n quit\n' | bluetoothctl
+mpg123 /home/pi/AcousticAsianCarp/rockytop.mp3
 
